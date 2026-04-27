349253030401d4c87cef50bf2b9d02a302f882fb
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-04-27 16:57:10 -0700

    step 5 completed

--- COMP003B.LectureActivity6/Program.cs
+++ COMP003B.LectureActivity6/Program.cs
@@ -1,7 +1,13 @@
+using COMP003B.LectureActivity6.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<WebDevAcademyContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
